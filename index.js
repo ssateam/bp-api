4756f018d6d44ec3d5582b61536c7c5813019f4a
Pavel Nachigin <npa@ooossa.ru>
2019-11-18 10:54:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+const axios = require('axios');
+const qs = require('qs');
+const _ = require('lodash');
+
+class BP {
+    constructor(domen, login, password, protocol = 'https', timeout = 30000) {
+        if (!domen) throw new Error(`domen can't be empty`);
+        if (!login) throw new Error(`login can't be empty`);
+        if (!password) throw new Error(`password can't be empty`);
+
+        this.login = login;
+        this.password = password;
+        this.timeout = timeout;
+        this.baseUrl = `${protocol}://${domen}/api/v1`;
+    }   
+
+    _getUrl(opt) {
+        switch (opt.resource) {
+            case 'record':
+                return `${this.baseUrl}/catalogs/${opt.catalogId}/records/${opt.recordId ? opt.recordId : ''}`;
+            case 'catalog':
+                return `${this.baseUrl}/catalogs/${opt.catalogId ? opt.catalogId : ''}`;
+            case 'section':
+                return `${this.baseUrl}/sections/${opt.sectionId}`;
+            case 'view':
+                return `${this.baseUrl}/catalogs/${opt.catalogId}/views/${opt.viewId}`;
+            case 'board':
+                return `${this.baseUrl}/boards/${opt.boardId}/widgets/${opt.widgetId}/${opt.type}`;
+            case 'file':
+                return `${this.baseUrl}/files/`;
+        }
+    }
+    async _request(url, method, data = {}, params = {}) {
+        return await axios({
+            auth: {
+                username: this.login,
+                password: this.password
+            },
+            timeout: this.timeout,
+            url: url,
+            method: method,
+            params: params,
+            paramsSerializer: function (params) {
+                return qs.stringify(params)
+            },
+            headers: {
+                'Content-type': 'application/json'
+            },
+            data: {
+                values: data
+            }
+        });      
+    }
+
+    async getRecordById(catalogId, recordId) {
+        if(!recordId) throw new Error(`recordId is required`);
+        if(!catalogId) throw new Error(`catalogId is required`);
+        let url = this._getUrl({resource: 'record', catalogId: catalogId, recordId: recordId});
+        let response = await this._request(url, 'GET');
+        return response.data;
+    }
+    async getRecords(catalogId, params = {}) {
+        if(!catalogId) throw new Error(`catalogId is required`);
+        let url = this._getUrl({resource: 'record', catalogId: catalogId, recordId: ''});
+        let response = await this._request(url, 'GET', undefined, params);
+        return response.data;
+    }
+    async getCatalog(catalogId = '') {
+        let url = this._getUrl({resource: 'catalog', catalogId: catalogId});
+        let response = await this._request(url, 'GET');
+        return response.data;
+    }
+    async getSection(sectionId = '') {
+        let url = this._getUrl({resource: 'section', sectionId: sectionId});
+        let response = await this._request(url, 'GET');
+        return response.data;
+    }
+    async getView(catalogId, viewId = '') {
+        if(!catalogId) throw new Error(`catalogId is required`);
+        let url = this._getUrl({resource: 'view', viewId: viewId, catalogId: catalogId});
+        let response = await this._request(url, 'GET');
+        return response.data;
+    }
+    async getWidget(boardId, params = {}, widgetId = 'new', type = 'values') {
+        if(!boardId) throw new Error(`boardId is required`);
+        let url = this._getUrl({resource: 'board', boardId: boardId, widgetId: widgetId, type: type});
+        let response = await this._request(url, 'GET', undefined, params);
+        return response.data;
+    }
+    async postRecord(catalogId, data = {}) {
+        if(!catalogId) throw new Error(`catalogId is required`);
+        if(typeof data != 'object') throw new Error(`data must be an object`);
+        let url = this._getUrl({resource: 'record', catalogId: catalogId});
+        let response = await this._request(url, 'POST', data);
+        return response.data;
+    }
+    async postCatalog(data = {}) {
+        if(typeof data != 'object') throw new Error(`data must be an object`);
+        if (_.isEmpty(data)) throw new Error(`data cant't be empty`);
+        let url = this._getUrl({resource: 'catalog'})
+        let response = await this._request(url, 'POST', data);
+        return response.data;
+    }
+    async postSection(data = {}) {
+        if(typeof data != 'object') throw new Error(`data must be an object`);
+        if (_.isEmpty(data)) throw new Error(`data cant't be empty`);
+        let url = this._getUrl({resource: 'section'});
+        let response = await this._request(url, 'POST', data);
+        return response.data;
+    }
+    async patchRecord(catalogId, recordId, data = {}) {
+        if(!catalogId) throw new Error(`catalogId is required`);
+        if(!recordId) throw new Error(`recordId is required`);
+        if(typeof data != 'object') throw new Error(`data must be an object`);
+        if (_.isEmpty(data)) throw new Error(`data cant't be empty`);
+        let url = this._getUrl({resource: 'record', catalogId: catalogId, recordId: recordId});
+        let response = await this._request(url, 'PATCH', data);
+        return response.data;
+    }
+    async patchCatalog(catalogId, data = {}) {
+        if(!catalogId) throw new Error(`catalogId is required`);
+        if(typeof data != 'object') throw new Error(`data must be an object`);
+        if (_.isEmpty(data)) throw new Error(`data cant't be empty`);
+        let url = this._getUrl({resource: 'catalog', catalogId: catalogId});
+        let response = await this._request(url, 'PATCH', data);
+        return response.data;
+    }
+    async patchSection(sectionId, data = {}) {
+        if(!catalogId) throw new Error(`sectionId is required`);
+        if(typeof data != 'object') throw new Error(`data must be an object`);
+        if (_.isEmpty(data)) throw new Error(`data cant't be empty`);
+        let url = this._getUrl({resource: 'section', sectionId: sectionId});
+        let response = await this._request(url, 'PATCH', data);
+        return response.data;
+    }
+    async deleteRecord(catalogId, recordId) {
+        if(!catalogId) throw new Error(`catalogId is required`);
+        if(!recordId) throw new Error(`recordId is required`);
+        let url = this._getUrl({resource: 'record', catalogId: catalogId, recordId: recordId});
+        let response = await this._request(url, 'DELETE');
+        return response.data;
+    }
+    async deleteCatalog(catalogId) {
+        console.log(`function is disabled!`);
+        return;
+        if(!catalogId) throw new Error(`catalogId is required`);
+        let url = this._getUrl({resource: 'record', catalogId: catalogId});
+        let response = await this._request(url, 'DELETE');
+        return response.data;
+    }
+    async deleteSection(sectionId) {
+        console.log(`function is disabled!`);
+        return;
+        if(!catalogId) throw new Error(`sectionId is required`);
+        let url = this._getUrl({resource: 'section', sectionId: sectionId});
+        let response = await this._request(url, 'DELETE');
+        return response.data;
+    }
+}
+
+module.exports = BP;
