591d3805acdad529f1bc581428d2b1eda2efe2ca
Pavel Nachigin <npa@ooossa.ru>
2022-06-22 06:24:01 +0000

    get realtion fix

--- index.js
+++ index.js
@@ -60,35 +60,35 @@ class BP {
   async getRecordById(catalogId, recordId, params = {}) {
     if (!recordId) throw new Error(`recordId is required`)
     if (!catalogId) throw new Error(`catalogId is required`)
-    let url = this._getUrl({ resource: 'record', catalogId: catalogId, recordId: recordId })
+    let url = this._getUrl({ resource: 'record', catalogId, recordId })
     let response = await this._request(url, 'GET', undefined, params)
     return response.data
   }
   async getRecords(catalogId, params = {}) {
     if (!catalogId) throw new Error(`catalogId is required`)
-    let url = this._getUrl({ resource: 'record', catalogId: catalogId, recordId: '' })
+    let url = this._getUrl({ resource: 'record', catalogId, recordId: '' })
     let response = await this._request(url, 'GET', undefined, params)
     return response.data
   }
   async getCatalog(catalogId = '') {
-    let url = this._getUrl({ resource: 'catalog', catalogId: catalogId })
+    let url = this._getUrl({ resource: 'catalog', catalogId })
     let response = await this._request(url, 'GET')
     return response.data
   }
   async getSection(sectionId = '') {
-    let url = this._getUrl({ resource: 'section', sectionId: sectionId })
+    let url = this._getUrl({ resource: 'section', sectionId })
     let response = await this._request(url, 'GET')
     return response.data
   }
   async getView(catalogId, viewId = '') {
     if (!catalogId) throw new Error(`catalogId is required`)
-    let url = this._getUrl({ resource: 'view', viewId: viewId, catalogId: catalogId })
+    let url = this._getUrl({ resource: 'view', viewId, catalogId })
     let response = await this._request(url, 'GET')
     return response.data
   }
   async getWidget(boardId, params = {}, widgetId = 'new', type = 'values') {
     if (!boardId) throw new Error(`boardId is required`)
-    let url = this._getUrl({ resource: 'board', boardId: boardId, widgetId: widgetId, type: type })
+    let url = this._getUrl({ resource: 'board', boardId, widgetId, type })
     let response = await this._request(url, 'GET', undefined, params)
     return response.data
   }
@@ -102,21 +102,21 @@ class BP {
   }
   async getValues(catalogId, params = {}) {
     if (!catalogId) throw new Error(`catalogId is required`)
-    let url = this._getUrl({ resource: 'values', catalogId: catalogId })
+    let url = this._getUrl({ resource: 'values', catalogId })
     let response = await this._request(url, 'GET', undefined, params)
     return response.data
   }
   async getRelations(catalogId, recordId, params = {}) {
     if (!catalogId) throw new Error(`catalogId is required`)
     if (!recordId) throw new Error(`recordId is required`)
-    let url = this._getUrl({ resource: 'relations', catalogId: catalogId, recordId, recordId })
+    let url = this._getUrl({ resource: 'relations', catalogId, recordId })
     let response = await this._request(url, 'GET', undefined, params)
     return response.data
   }
   async postRecord(catalogId, data = {}) {
     if (!catalogId) throw new Error(`catalogId is required`)
     if (typeof data != 'object') throw new Error(`data must be an object`)
-    let url = this._getUrl({ resource: 'record', catalogId: catalogId })
+    let url = this._getUrl({ resource: 'record', catalogId })
     let response = await this._request(url, 'POST', { values: data })
     return response.data
   }
@@ -139,7 +139,7 @@ class BP {
     if (!recordId) throw new Error(`recordId is required`)
     if (typeof data != 'object') throw new Error(`data must be an object`)
     if (_.isEmpty(data)) throw new Error(`data cant't be empty`)
-    let url = this._getUrl({ resource: 'record', catalogId: catalogId, recordId: recordId })
+    let url = this._getUrl({ resource: 'record', catalogId, recordId })
     let response = await this._request(url, 'PATCH', { values: data })
     return response.data
   }
@@ -147,7 +147,7 @@ class BP {
     if (!catalogId) throw new Error(`catalogId is required`)
     if (typeof data != 'object') throw new Error(`data must be an object`)
     if (_.isEmpty(data)) throw new Error(`data cant't be empty`)
-    let url = this._getUrl({ resource: 'catalog', catalogId: catalogId })
+    let url = this._getUrl({ resource: 'catalog', catalogId })
     let response = await this._request(url, 'PATCH', data)
     return response.data
   }
@@ -155,7 +155,7 @@ class BP {
     if (!sectionId) throw new Error(`sectionId is required`)
     if (typeof data != 'object') throw new Error(`data must be an object`)
     if (_.isEmpty(data)) throw new Error(`data cant't be empty`)
-    let url = this._getUrl({ resource: 'section', sectionId: sectionId })
+    let url = this._getUrl({ resource: 'section', sectionId })
     let response = await this._request(url, 'PATCH', data)
     return response.data
   }
@@ -164,14 +164,14 @@ class BP {
     if (!viewId) throw new Error(`viewId is required`)
     if (typeof data != 'object') throw new Error(`data must be an object`)
     if (_.isEmpty(data)) throw new Error(`data cant't be empty`)
-    let url = this._getUrl({ resource: 'view', catalogId: catalogId, viewId: viewId })
+    let url = this._getUrl({ resource: 'view', catalogId, viewId })
     let response = await this._request(url, 'PATCH', data)
     return response.data
   }
   async deleteRecord(catalogId, recordId) {
     if (!catalogId) throw new Error(`catalogId is required`)
     if (!recordId) throw new Error(`recordId is required`)
-    let url = this._getUrl({ resource: 'record', catalogId: catalogId, recordId: recordId })
+    let url = this._getUrl({ resource: 'record', catalogId, recordId })
     let response = await this._request(url, 'DELETE')
     return response.data
   }
@@ -179,7 +179,7 @@ class BP {
     console.log(`function is disabled!`)
     return
     if (!catalogId) throw new Error(`catalogId is required`)
-    let url = this._getUrl({ resource: 'record', catalogId: catalogId })
+    let url = this._getUrl({ resource: 'record', catalogId })
     let response = await this._request(url, 'DELETE')
     return response.data
   }
@@ -187,7 +187,7 @@ class BP {
     console.log(`function is disabled!`)
     return
     if (!catalogId) throw new Error(`sectionId is required`)
-    let url = this._getUrl({ resource: 'section', sectionId: sectionId })
+    let url = this._getUrl({ resource: 'section', sectionId })
     let response = await this._request(url, 'DELETE')
     return response.data
   }
